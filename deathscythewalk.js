1fb479c9392593c3e0b38fbd7d1c8f026725fcfb
Michael <michaeltheisen@hotmail.com>
2023-01-08 06:12:31 -0800

    letter

--- deathscythewalk.js
+++ deathscythewalk.js
@@ -2,9 +2,9 @@ class Deathscythewalk {
     constructor(game) {
         this.game = game;   
         this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprites/deathscythewalk.png"), 
-        0, 0, 80, 114, 12, 0.5);
-        // the width of the deathscyth in pixels is 80 pixels 
-        // the height of the deathscyth in pixels is 114 pixels
+        0, 0, 80, 114, 12, 0.0);
+        // the width of the deathscythe in pixels is 80 pixels 
+        // the height of the deathscythe in pixels is 114 pixels
 
     };
 
