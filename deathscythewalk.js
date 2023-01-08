506e3fd696cedc1ccd03fa9e41e6f07c79b152c8
Michael Theisen <60194610+MJTheisen@users.noreply.github.com>
2023-01-08 06:24:03 -0800

    Update deathscythewalk.js

--- deathscythewalk.js
+++ deathscythewalk.js
@@ -2,7 +2,7 @@ class Deathscythewalk {
     constructor(game) {
         this.game = game;   
         this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprites/deathscythewalk.png"), 
-        0, 0, 80, 114, 12, 0.0);
+        0, 0, 80, 114, 12, 0.1);
         // the width of the deathscythe in pixels is 80 pixels 
         // the height of the deathscythe in pixels is 114 pixels
 
