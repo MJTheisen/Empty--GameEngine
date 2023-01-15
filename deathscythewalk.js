4144bf9c132f36bec5574370e41d603288622ac1
Michael <michaeltheisen@hotmail.com>
2023-01-15 01:52:13 -0800

    fixed sprite

--- deathscythewalk.js
+++ deathscythewalk.js
@@ -2,7 +2,7 @@ class Deathscythewalk {
     constructor(game) {
         this.game = game;   
         this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprites/deathscythewalk.png"), 
-        0, 0, 80, 114, 12, 0.15); //must have value for framDuration
+        -0.25, 0, 81, 114, 12, 0.15); //must have value for framDuration
         // the width of the deathscythe in pixels is 80 pixels. 
         // the height of the deathscythe in pixels is 114 pixels.
         // the spritesheet was made with bad overlaps though, so I need to fix it manually.
